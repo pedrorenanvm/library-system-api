b6117b5b203b2db8e383d562ef08eba92cec6e27
LuizFelipePSilva <LuizFelipeP.S@outlook.com.br>
2026-06-09 10:30:46 -0300

    feat: add method findAll in getFilters

--- src/modules/title/domain/repositories/ITitleRepository.ts
+++ src/modules/title/domain/repositories/ITitleRepository.ts
@@ -1,11 +1,13 @@
-import { ICreateTitle } from "../models/ICreateTitle";  
-import { ITitle } from "../models/ITitle";
+import { ICreateTitle } from '../models/ICreateTitle';
+import { IGetTitleFilters } from '../models/IGetTitleFilters';
+import { ITitle } from '../models/ITitle';
+import { ITitlePaginate } from '../models/ITitlePaginate';
 
 export interface ITitleRepository {
-    findById(id: string): Promise<ITitle | null>;
-    findByName(name: string): Promise<ITitle | null>;
-    create(data: ICreateTitle): Promise<ITitle>;
-    update(title: ITitle): Promise<ITitle>;
-    delete(id: string): Promise<void>;
+  findById(id: string): Promise<ITitle | null>;
+  findByName(name: string): Promise<ITitle | null>;
+  findAll(data: IGetTitleFilters): Promise<ITitlePaginate>;
+  create(data: ICreateTitle): Promise<ITitle>;
+  update(title: ITitle): Promise<ITitle>;
+  delete(id: string): Promise<void>;
 }
-
--- src/modules/title/infra/typeorm/repositories/TitleRepository.ts
+++ src/modules/title/infra/typeorm/repositories/TitleRepository.ts
@@ -1,45 +1,63 @@
-import { Title } from "@modules/title/infra/typeorm/entities/Title";
-import { ITitleRepository } from "../../../domain/repositories/ITitleRepository";
-import { Repository } from "typeorm";
-import { AppDataSource } from "@shared/infra/typeorm/dataSource";
-import { ITitle } from "@modules/title/domain/models/ITitle";
-import { ICreateTitle } from "@modules/title/domain/models/ICreateTitle";
+import { Title } from '@modules/title/infra/typeorm/entities/Title';
+import { ITitleRepository } from '../../../domain/repositories/ITitleRepository';
+import { Repository } from 'typeorm';
+import { ITitle } from '@modules/title/domain/models/ITitle';
+import { ICreateTitle } from '@modules/title/domain/models/ICreateTitle';
+import { IGetTitleFilters } from '@modules/title/domain/models/IGetTitleFilters';
+import { ITitlePaginate } from '@modules/title/domain/models/ITitlePaginate';
 
 export class TitleRepository implements ITitleRepository {
+  private ormRepository: Repository<Title>;
 
-    private ormRepository: Repository<Title>;
+  constructor() {
+    this.ormRepository = AppDataSource.getRepository(Title);
+  }
 
+  async findAll(data: IGetTitleFilters): Promise<ITitlePaginate> {
+    const { page = 0, limit = 0, type } = data;
 
-    constructor() {
-        this.ormRepository = AppDataSource.getRepository(Title);
+    const query = this.ormRepository.createQueryBuilder('title');
 
+    if (type) {
+      query.where('title.type = :type', { type });
     }
 
-    async findById(id: string): Promise<ITitle | null> {
+    const [dataTotal, total] = await query
+      .skip((page - 1) * limit)
+      .take(limit)
+      .getManyAndCount();
 
-        return await this.ormRepository.findOne({ where: { id } });
-    }
+    const result: ITitlePaginate = {
+      per_page: limit,
+      data: dataTotal,
+      total,
+      current_page: page,
+      last_page: Math.ceil(total / limit),
+    };
+    return result;
+  }
 
-    async findByName(name: string): Promise<ITitle | null> {
+  async findById(id: string): Promise<ITitle | null> {
+    return await this.ormRepository.findOne({ where: { id } });
+  }
 
-        return await this.ormRepository.findOne({ where: { name } });
-    }
+  async findByName(name: string): Promise<ITitle | null> {
+    return await this.ormRepository.findOne({ where: { name } });
+  }
 
-    async create(data: ICreateTitle): Promise<ITitle> {
+  async create(data: ICreateTitle): Promise<ITitle> {
+    const title = this.ormRepository.create(data);
+    await this.ormRepository.save(title);
+    return title;
+  }
 
-        const title = this.ormRepository.create(data);
-        await this.ormRepository.save(title);
-        return title;
-        
-    }
+  async update(title: ITitle): Promise<ITitle> {
+    await this.ormRepository.save(title);
+    return title;
+  }
 
-    async update(title: ITitle): Promise<ITitle> {
-        await this.ormRepository.save(title);
-        return title;
-    }
-    
-    async delete(id: string): Promise<void> {
-        await this.ormRepository.softDelete(id);
-    }
-
+  async delete(id: string): Promise<void> {
+    await this.ormRepository.softDelete(id);
+  }
 }
+
